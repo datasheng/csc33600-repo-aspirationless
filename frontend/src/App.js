633c138631d19e56c42216d7cda8fb6591fce39a
Edison222 <121990442+Edison222@users.noreply.github.com>
2025-04-26 21:10:16 -0400

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './home.css'; // Import the CSS
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-card">
+        <h1 className="home-title">Welcome to Price Scout</h1>
+        <p className="home-subtitle">Find the best prices across all stores easily!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
