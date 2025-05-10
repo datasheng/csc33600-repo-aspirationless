1ba232d4f0f66647a5451ab37ccde37f37754502
Edison222 <121990442+Edison222@users.noreply.github.com>
2025-05-09 23:09:33 -0400

    Home page

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import userRoutes from './routes/users.js';
+import productRoutes from './routes/products.js'; // Import the product routes
 
 dotenv.config();
 
@@ -9,7 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Use the routes
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);  // Add this line
 
 app.listen(8800, () => {
     console.log('Backend server is running!');
